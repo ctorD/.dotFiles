4a77d5a843ba76daf7be7704c927bdd4962dd430
DC <danclarke98@gmail.com>
2025-09-05 23:18:59 +0100

    Updated wezterm config

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -15,7 +15,13 @@ config.initial_rows = 28
 config.font_size = 10
 config.color_scheme = 'catppuccin-macchiato'
 config.font = wezterm.font 'JetBrains Mono'
+
+
 -- Finally, return the configuration to wezterm:
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.93
+config.window_decorations = "RESIZE"
+config.max_fps = 120
+config.prefer_egl = true
+config.debug_key_events = true
 return config
