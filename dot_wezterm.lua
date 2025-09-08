83c1d43ae533047bb24d9785a11b913d7ed41b05
DC <danclarke98@gmail.com>
2025-09-08 22:09:27 +0100

    Added YASB config

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -9,8 +9,8 @@ config.default_prog = { "pwsh.exe", "-NoLogo" }
 
 -- For example, changing the initial geometry for new windows:
 -- TODO: Calculate Rows/Cols for screen
-config.initial_cols = 120
-config.initial_rows = 60
+config.initial_cols = 1
+config.initial_rows = 1
 
 -- or, changing the font size and color scheme.
 config.font_size = 10
@@ -19,7 +19,7 @@ config.font = wezterm.font("JetBrains Mono")
 
 -- Finally, return the configuration to wezterm:
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.99
+-- config.window_background_opacity = 0.99
 config.window_decorations = "RESIZE"
 config.max_fps = 120
 config.prefer_egl = true
